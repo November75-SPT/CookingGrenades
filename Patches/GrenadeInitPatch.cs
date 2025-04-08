0080754c19e2c00634f49f5e76a9b330d316df23
November75 <96359089+Hotel2622@users.noreply.github.com>
2025-04-08 20:25:18 +0900

    Fixed impact fuse not syncing with cooking time by setting float_3 in Grenade.Init

--- Patches/GrenadeInitPatch.cs
+++ Patches/GrenadeInitPatch.cs
@@ -11,18 +11,16 @@ using System.Threading.Tasks;
 using EFT;
 using SPT.Custom.Utils;
 
-namespace CookingGrenades.Patches
+namespace CookingGrenades.Patches;
+public class GrenadeInitPatch : ModulePatch
 {
-    public class GrenadeInitPatch : ModulePatch
+    protected override MethodBase GetTargetMethod()
     {
-        protected override MethodBase GetTargetMethod()
-        {
-            return AccessTools.DeclaredMethod(typeof(Grenade), nameof(Grenade.Init));
-        }
-        [PatchPostfix]
-        public static void PatchPostfix(Grenade __instance,float timeSpent, float __state)
-        {
-            // Plugin.log.LogInfo($"Grenade {__instance.Player.Nickname} {timeSpent}");
-        }
+        return AccessTools.DeclaredMethod(typeof(Grenade), nameof(Grenade.Init));
+    }
+    [PatchPostfix]
+    public static void PatchPostfix(Grenade __instance, float timeSpent)
+    {
+        Traverse.Create(__instance).Field("float_3").SetValue(timeSpent);
     }
 }
